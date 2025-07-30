2259c72d99c4475808ea57b26ea3c7665a73dc33
NACP <183621418+agfitgb@users.noreply.github.com>
2025-07-30 16:15:14 +0900

    🚀 Add working Render-ready Flask app with PostgreSQL

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DATABASE_URL="postgresql://nacp_user:lvZjFz6BVT2kDEv3KKKKZ8J7mnJi30bb@dpg-d24s71fgi27c73bcq4l0-a.oregon-postgres.render.com/nacp"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("NoMoreAntiSocialPartyInTheFreeKorea!!20250836", "dev")
